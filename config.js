373b0a5996a23627155b19ffe4e5548b13e1be4b
SahiMilo <sahimilo.in@gmail.com>
2026-09-18 01:18:31 +0530

    Add Supabase browser configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+// Public browser configuration. Never put a service_role key here.
+window.SAHIMILO_CONFIG={
+  supabaseUrl:"https://sixcvnydpdngtkcthvor.supabase.co",
+  supabasePublishableKey:"sb_publishable_TnBZn126eibqJnzNVjxqAw_DHVcyRHn",
+  whatsappNumber:"919999999999"
+};
